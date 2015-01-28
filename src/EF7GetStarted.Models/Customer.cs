5dc5f481992665b3c8fba46bd652917957bdac15
shahedurrahman <shahed.munna@gmail.com>
2015-01-28 12:31:12 +0600

    C

--- src/EF7GetStarted.Models/Customer.cs
+++ src/EF7GetStarted.Models/Customer.cs
@@ -4,6 +4,6 @@ namespace EF7GetStarted.Models
     public class Customer
     {
         public int Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
     }
 }
--- src/EF7GetStarted/Controllers/CustomerController.cs
+++ src/EF7GetStarted/Controllers/CustomerController.cs
@@ -1,22 +1,40 @@
 ﻿using EF7GetStarted.Models;
 using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
 
 namespace EF7GetStarted.Controllers
 {
 	[Route("api/[controller]")]
 	public class CustomerController : Controller
 	{
-		[HttpGet]
-		public IActionResult Get()
-		{
-			var customer = new Customer();
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 101,
+                    Name="Customer 101"
+                },
+                new Customer
+                {
+                    Id = 102,
+                    Name="Customer 102"
+                },
+                new Customer
+                {
+                    Id = 103,
+                    Name="Customer 103"
+                },
+                new Customer
+                {
+                    Id = 104,
+                    Name="Customer 104"
+                }
+            };
 
-			return new ObjectResult(customer);
-		}
-
-		public void Test()
-		{
-			
-		}
-	}
+            return new ObjectResult(customers);
+        }
+    }
 }
